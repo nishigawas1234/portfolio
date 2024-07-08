ad4ee6a52bf722117b8bb318a9958587b0d247d8
NishigandhaGawas <129956160+NishigandhaGawas@users.noreply.github.com>
2024-07-09 02:08:30 +0530

    feat: home page

---
+++ src/components/Sections/Skill.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Skill() {
+  return (
+    <div className="px-20 w-full pt-[100px]">
+      <div className="text-[65px] text-center font-semibold">Skills</div>
+      <div className="mt-[60px]"></div>
+    </div>
+  );
+}
---
+++ src/components/SkillBox.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SkillBox() {
+  return <div>SkillBox</div>;
+}
