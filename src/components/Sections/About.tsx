f27c28933cd0caa9eeafa3d824d76cfbb5c7aacf
NishigandhaGawas <129956160+NishigandhaGawas@users.noreply.github.com>
2024-07-09 10:53:40 +0530

    fix: build

--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -7,14 +7,14 @@ export default function About() {
         <div className="w-[60%] p-10">
           <div className="text-[65px] font-semibold">About Me</div>
           <div className="text-[16px] font-normal">
-            A frontend developer who's passionate about crafting beautiful and
+            A frontend developer who is passionate about crafting beautiful and
             user-friendly websites. With 2+ years of experience in the field, I
             specialize in using a variety of frontend frameworks such as
-            React.js and Next.js to bring designs to life. Whether it's building
-            interactive components, optimizing performance, or ensuring
+            React.js and Next.js to bring designs to life. Whether it is
+            building interactive components, optimizing performance, or ensuring
             cross-browser compatibility, I love diving into the world of
-            frontend development. I'm always on the lookout for new challenges
-            and opportunities to expand my skills. Let's collaborate and create
+            frontend development. I am always on the lookout for new challenges
+            and opportunities to expand my skills. Let s collaborate and create
             something awesome together!
           </div>
         </div>
